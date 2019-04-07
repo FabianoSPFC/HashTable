766b44e8f8ea17c878e7b3ae2395d996aa278638
FabianoSPFC <programador.workspace@gmail.com>
2019-04-07 20:44:17 -0300

    Interface para Tabela hash

--- Hashable.java
+++ Hashable.java
@@ -1,17 +1,7 @@
+// Programação Feita por Fabiano Filho e Rodrigo Melo
+
 package atividade_pesquisa;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Scanner;
-
-/**
- *
- * @author Fabiano Filho
- */
 public interface Hashable {
 
   
